d768e3e1556e447502aeaf547383b2298c0dde7c
Stepan Hojdar <hojdar.stepan@gmail.com>
2024-07-07 21:40:28 +0200

    Synchronize results bitfield access

--- bittorrent/peer_protocol.go
+++ bittorrent/peer_protocol.go
@@ -153,7 +153,9 @@ func deserializeHandshake(reader io.Reader) (HandshakeData, error) {
 }
 
 func sendBitfield(conn net.Conn, results *types.Results) error {
+	results.Lock.Lock()
 	bitfieldMsg := PeerMessage{KeepAlive: false, Code: MsgBitfield, Data: results.Bitfield.Bytes()}
+	results.Lock.Unlock()
 	bfMsgBytes, err := SerializeMessage(bitfieldMsg)
 	if err != nil {
 		return fmt.Errorf("error while serializing bitfield message to target=%s, err=%s", conn.RemoteAddr().String(), err)
