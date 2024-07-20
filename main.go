f03ed117ed295218053a7b7d90e8be2a35515ba9
Stepan Hojdar <hojdar.stepan@gmail.com>
2024-07-20 21:54:10 +0200

    Change main.go to slog

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"log/slog"
 	"math/rand"
 	"net"
@@ -36,20 +35,21 @@ const Port uint16 = 6881
 func listeningServer(ctx context.Context, torrent *types.TorrentFile, comms types.Communication, results *types.Results, conns *Connections) {
 	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", Port))
 	if err != nil {
-		log.Fatalf("encountered error listening on port=%d, error=%s", Port, err)
+		slog.Error("error while listening on port", slog.Int("port", int(Port)), slog.String("err", err.Error()))
+		os.Exit(1)
 	}
 
 	for {
 		conn, err := listener.Accept()
-		log.Printf("incoming connection, addr=%s", conn.RemoteAddr().String())
+		slog.Info("incoming connection", slog.String("addr", conn.RemoteAddr().String()))
 		if err != nil {
-			log.Printf("ERROR: accept failed, err=%s", err)
+			slog.Error("error while accepting connection", slog.String("addr", conn.RemoteAddr().String()), slog.String("err", err.Error()))
 			continue
 		}
 
 		connection, err := conns.Add(conn.RemoteAddr())
 		if err != nil {
-			log.Printf("ERROR: adding a connection to pool failed, error=%s", err)
+			slog.Error("adding a connection to pool failed", slog.String("addr", conn.RemoteAddr().String()), slog.String("err", err.Error()))
 			continue
 		}
 
@@ -66,7 +66,7 @@ func listeningServer(ctx context.Context, torrent *types.TorrentFile, comms type
 }
 
 func connectToPeer(ctx context.Context, torrent *types.TorrentFile, comms types.Communication, results *types.Results, ip string, port uint16, conns *Connections) error {
-	log.Printf("connecting to %s", ip)
+	slog.Debug("connecting to peer", slog.String("ip", ip))
 
 	isOpen, err := conns.IsOpen(ip)
 	if err != nil {
@@ -129,7 +129,7 @@ func loadFromFile(torrent types.TorrentFile, results *types.Results) (bool, erro
 	if fileErr == nil {
 		err := loadPiecesFromCompleteFile(fullFile, torrent, results)
 		if err == nil {
-			log.Printf("loaded %d pieces from file %s", results.PiecesDone, torrent.Name)
+			slog.Debug("loaded data from file", slog.Int("pieces", results.PiecesDone), slog.String("file", torrent.Name))
 			return true, nil
 		}
 	}
@@ -199,7 +199,7 @@ func loadPiecesFromPartialFiles(torrent types.TorrentFile, results *types.Result
 			}
 		}
 
-		log.Printf("loaded %d pieces from file %s", numberOfPiecesInFile, filename)
+		slog.Debug("loaded data from file", slog.Int("pieces", numberOfPiecesInFile), slog.String("file", filename))
 	}
 	return nil
 }
@@ -212,7 +212,7 @@ func launchClients(requestedConnections int, ctx context.Context, torrent *types
 		// if already open -> skip
 		isOpen, e := conns.IsOpen(ip)
 		if e != nil {
-			log.Printf("ERROR: error while checking if already connected to IP=%s, err=%s, skipping", ip, e)
+			slog.Error("error while checking if already connected to IP, skipping", slog.String("ip", ip), slog.String("err", e.Error()))
 			continue
 		}
 		if isOpen {
@@ -222,7 +222,7 @@ func launchClients(requestedConnections int, ctx context.Context, torrent *types
 		// if blacklisted -> skip
 		isBlacklisted, eb := conns.IsBlacklisted(ip)
 		if eb != nil {
-			log.Printf("ERROR: error while checking if already connected to IP=%s, err=%s, skipping", ip, eb)
+			slog.Error("error while checking if IP is blacklisted, skipping", slog.String("ip", ip), slog.String("err", eb.Error()))
 			continue
 		}
 		if isBlacklisted {
@@ -231,10 +231,10 @@ func launchClients(requestedConnections int, ctx context.Context, torrent *types
 
 		err := connectToPeer(ctx, torrent, comms, results, ip, peerInfo.Ports[i], conns)
 		if err != nil {
-			log.Printf("ERROR: encountered an error connecting to target=%s, err=%s", peerInfo.IPs[i], err)
+			slog.Error("connecting to the target failed", slog.String("ip", peerInfo.IPs[i].String()), slog.String("err", err.Error()))
 		} else {
 			numberOfConnections += 1
-			log.Printf("connected to peer number %d, connected to %d peers", i, numberOfConnections)
+			slog.Debug("connection to peer successful", slog.Int("peer number", i), slog.Int("total connections", numberOfConnections))
 		}
 
 		if numberOfConnections == requestedConnections {
@@ -246,7 +246,7 @@ func launchClients(requestedConnections int, ctx context.Context, torrent *types
 	}
 
 	if numberOfConnections > 0 {
-		log.Printf("launched %d connections, connected to %d peers", numberOfConnections, conns.GetConnectedNumber())
+		slog.Debug("connections launched", slog.Int("launched", numberOfConnections), slog.Int("total", conns.GetConnectedNumber()))
 	}
 }
 
@@ -425,13 +425,14 @@ func updateOnlineConnections(connections *Connections, comms *types.Communicatio
 		select {
 		case ended, ok := <-comms.ConnectionEnded:
 			if ok {
-				log.Printf("detected connection closed, peer-id=%s, address=%s", ended.Id, ended.Addr.String())
+				slog.Info("detected a closed connection", slog.String("peer-id", ended.Id), slog.String("addr", ended.Addr.String()))
 				toRemove = append(toRemove, ended.Addr)
 				delete(*generosity, ended.Id)
 				delete(*interestedPeers, ended.Id)
 				connections.BlacklistAddr(ended.Addr)
 			} else {
-				log.Fatalf("ERROR: peer ended channel was closed")
+				slog.Error("peer ended channel was closed")
+				os.Exit(1)
 			}
 		default:
 			done = true
@@ -445,12 +446,12 @@ func updateOnlineConnections(connections *Connections, comms *types.Communicatio
 	for _, r := range toRemove {
 		err := connections.Remove(r)
 		if err != nil {
-			log.Printf("ERROR: error while removing connection=%s, err=%s", r.String(), err)
+			slog.Error("error while removing connection", slog.String("connection", r.String()), slog.String("err", err.Error()))
 		}
 	}
 
 	if toRemoveLen > 0 {
-		log.Printf("finished removing %d offline connections", toRemoveLen)
+		slog.Debug("all offline connections removed", slog.Int("removed", toRemoveLen))
 	}
 }
 
@@ -519,7 +520,7 @@ func main() {
 	logWriter := io.MultiWriter(f, os.Stderr)
 
 	logOptions := &slog.HandlerOptions{
-		Level:       slog.LevelInfo,
+		Level:       slog.LevelDebug,
 		AddSource:   false,
 		ReplaceAttr: nil,
 	}
@@ -530,52 +531,54 @@ func main() {
 
 	filename := os.Args[1]
 	if _, err := os.Stat(filename); err != nil {
-		log.Fatalf("ERROR: file does not exist, file=%s", filename)
+		slog.Error("file does not exist", slog.String("file", filename))
+		os.Exit(1)
 	}
 
 	slog.Info("started", "file", filename)
 
-	// log.Printf("started on file=%s\n", filename)
-
 	torrentFile, err := os.Open(filename)
 	if err != nil {
-		log.Fatalf("ERROR: cannot open file, err=%s", err)
+		slog.Error("error while opening a file", slog.String("file", filename), slog.String("err", err.Error()))
+		os.Exit(1)
 	}
 
 	torrent, err := decode.DecodeTorrentFile(torrentFile)
 	if err != nil {
-		log.Fatalf("ERROR: encountered an error during .torrent file decoding, err=%s", err)
+		slog.Error("error while decoding the .torrent file", slog.String("err", err.Error()))
+		os.Exit(1)
 	}
-
-	fmt.Println("10%")
 
 	if torrent.Length == 0 {
 		// TODO: specification requires either 'length' or 'key files', implement 'key files'
-		log.Fatalf("ERROR: key 'length' is missing, unsupported .torrent file")
+		slog.Error("unsupported .torrent file, key 'length' is missing")
+		os.Exit(1)
 	}
 
 	slog.Info("torrent file decoded", slog.String("file", torrent.Name), slog.String("size", humanize.Bytes(uint64(torrent.Length))), slog.Int("pieces", len(torrent.PieceHashes)))
-	// log.Printf("torrent file=%s, size=%s, pieces=%d", torrent.Name, humanize.Bytes(uint64(torrent.Length)), len(torrent.PieceHashes))
-
-	os.Exit(0)
 
 	// load the file or PartialFiles, verify all pieces hashes, create BitField
 	results := types.Results{Pieces: make([]*types.Piece, len(torrent.PieceHashes)), Bitfield: bitfield.New(len(torrent.PieceHashes)), Lock: sync.RWMutex{}}
 
 	alreadySavedFinalFile, err := loadFromFile(torrent, &results)
 	if err != nil {
-		log.Fatalf("ERROR: encountered an error while loading data from files, err=%s", err)
+		slog.Error("error while loading data from downloaded files", slog.String("err", err.Error()))
+		os.Exit(1)
 	}
 	alreadySavedNumber := results.PiecesDone
 	alreadySavedPieces := bitfield.Copy(&results.Bitfield)
 
 	peerId := bittorrent.MakePeerId()
+	slog.Debug("peer-id generated", slog.String("peer-id", peerId))
+
 	leftToDownload := torrent.Length - results.PiecesDone*torrent.PieceLength
 	trackerInfo, err := bittorrent.GetTrackerFromFile(torrent, peerId, Port, 0, 0, leftToDownload)
 	if err != nil {
-		log.Fatalf("ERROR: encountered an error while retrieving peers from tracker, err=%s", err)
+		slog.Error("error while retrieving peers from tracker", slog.String("err", err.Error()))
+		os.Exit(1)
 	}
-	log.Printf("set peer-id to=%s, received %d peers, interval=%d", peerId, len(trackerInfo.IPs), trackerInfo.Interval)
+
+	slog.Info("received response from tracker", slog.Int("peers", len(trackerInfo.IPs)), slog.Int("interval", trackerInfo.Interval))
 
 	sharedComms := types.Communication{
 		Orders:          make(chan *types.PieceOrder, len(torrent.PieceHashes)),
@@ -588,7 +591,8 @@ func main() {
 	for _, r := range rand.Perm(torrent.GetNumberOfPieces()) {
 		have, err := results.Bitfield.Get(r)
 		if err != nil {
-			log.Fatalf("ERROR: encountered error while checking bitfield, err=%s", err)
+			slog.Error("error while checking bitfield", slog.String("err", err.Error()))
+			os.Exit(1)
 		}
 		if have {
 			continue
@@ -620,7 +624,7 @@ func main() {
 
 		select {
 		case <-signalCh:
-			log.Printf("SIGINT caught, terminating")
+			slog.Info("SIGINT caught, terminating")
 			cancel()
 			time.Sleep(time.Second)
 			exit = true
@@ -630,7 +634,7 @@ func main() {
 			results.Lock.Unlock()
 			if piecesDone != len(torrent.PieceHashes) {
 				piecePercent := 100.0 * float32(piecesDone) / float32(len(torrent.PieceHashes))
-				log.Printf("downloaded %d/%d pieces, %f%%, uploaded %d pieces", piecesDone, len(torrent.PieceHashes), piecePercent, uploadedPieces)
+				fmt.Printf("downloaded %d/%d pieces, %f%%, uploaded %d pieces\n", piecesDone, len(torrent.PieceHashes), piecePercent, uploadedPieces)
 			}
 		case piece := <-sharedComms.Results:
 			results.Lock.Lock()
@@ -639,7 +643,7 @@ func main() {
 			err := results.Bitfield.Set(piece.Index, true)
 			results.Lock.Unlock()
 			if err != nil {
-				log.Fatalf("ERROR: encountered an error while setting a bit in bitfield to true, index=%d, err=%s", piece.Index, err)
+				slog.Error("encountered an error while setting a bit in bitfield to true", slog.Int("index", piece.Index), slog.String("err", err.Error()))
 			}
 			_, found := generosityMap[piece.DownloadedFromId]
 			if found {
@@ -654,10 +658,10 @@ func main() {
 				case peer.peersToUnchokeCh <- unchokedPeers:
 					continue
 				default:
-					log.Printf("WARN: peer=%s is not reading their choke-algorithm channel", peer.ip)
+					slog.Warn("peer is not reading their choke-algorithm channel", slog.String("peer", peer.ip.String()))
 				}
 			}
-			log.Printf("choke algorithm tick complete, unchoking=%v, generosity=%v, interest=%v", unchokedPeers, generosityMap, interestedPeers)
+			slog.Info("choke algorithm tick complete", "unchoking", unchokedPeers, "generosity", generosityMap, "interest", interestedPeers)
 		case <-trackerUpdateCh:
 			results.Lock.Lock()
 			uploaded := uploadedPieces * torrent.PieceLength
@@ -668,17 +672,19 @@ func main() {
 			if err != nil {
 				trackerInfo, err = bittorrent.GetTrackerFromFile(torrent, peerId, Port, uploaded, downloaded, leftToDownload)
 				if err != nil {
-					log.Printf("ERROR: encountered a fatal error while updating tracker, exiting, err=%s", err)
 					exit = true // cannot get a tracker -> unrecoverable error -> shutdown
+					slog.Error("encountered a fatal error while updating tracker, exiting", slog.String("err", err.Error()))
 				}
 			}
-			log.Printf("tracker updated, uploaded=%d, downloaded=%d, left-to-download=%d", uploaded, downloaded, leftToDownload)
+			slog.Info("tracker updated", slog.Int("uploaded", uploaded), slog.Int("downloaded", downloaded), slog.Int("left to download", leftToDownload))
 		case interestedPeer := <-sharedComms.PeerInterested:
 			interestedPeers[interestedPeer.Id] = interestedPeer.IsInterested
-			log.Printf("interested peer registered, id=%s, isInterested=%t", interestedPeer.Id, interestedPeer.IsInterested)
+			if interestedPeer.IsInterested {
+				slog.Info("interested peer detected", slog.String("peer-id", interestedPeer.Id))
+			}
 		case <-sharedComms.Uploaded:
 			uploadedPieces += 1
-			log.Printf("piece uploaded, total uploaded pieces=%d", uploadedPieces)
+			slog.Debug("piece uploaded", slog.Int("total uploaded pieces", uploadedPieces))
 		}
 
 		updateOnlineConnections(&connections, &sharedComms, &generosityMap, &interestedPeers)
@@ -695,21 +701,21 @@ func main() {
 			alreadySavedFinalFile = true
 			resultFile, err := os.Create(torrent.Name)
 			if err != nil {
-				log.Printf("ERROR: encountered error while writing result file, err=%s", err)
+				slog.Error("encountered error while writing result file", slog.String("file", torrent.Name), slog.String("err", err.Error()))
 			}
 			results.Lock.RLock()
 			file.Save(resultFile, results.Pieces)
 			results.Lock.RUnlock()
-			log.Printf("file saved, name=%s", torrent.Name)
+			slog.Info("file saved", slog.String("filename", torrent.Name))
 		}
 	}
 
 	// if the partial files were incomplete at start, save them
 	if alreadySavedNumber < torrent.GetNumberOfPieces() {
-		log.Printf("saving %d pieces", results.PiecesDone)
+		slog.Info("saving pieces to partial files", slog.Int("pieces", results.PiecesDone))
 		err = savePartialFiles(torrent, &results, &alreadySavedPieces)
 		if err != nil {
-			log.Printf("ERROR: encountered error while saving partial files, err=%s", err)
+			slog.Error("encountered error while saving partial files", slog.String("err", err.Error()))
 		}
 	}
 
